9abb30d504428eaa2e97d6a53ee5af28529fca76
jatinsajwan3841 <jatinsajwan3841@gmail.com>
2022-01-11 13:26:52 +0530

    updated changes

--- src/components/workEx/data.js
+++ src/components/workEx/data.js
@@ -2,8 +2,8 @@ export const WORKEX_DATA = [
     {
         role: "Software Development Intern",
         company: "Evolv Fit",
-        desc: "Developed dashboard, integrated mediapipe solutions in UI.",
-        tech: ["ReactJs", "JavaScript", "HTML", "CSS", "Bootstrap", "Redux"],
+        desc: "It was 2 months long remote internship in which I developed dashboard and integrated mediapipe solutions in React. The dashboard will help the users to stay fit by providing all key metrics and relevant details. With the mediapipe solutions, although the model was developed by ML team in python, I integrated it in React which will help users to have guided exercises.",
+        tech: ["ReactJs", "JavaScript", "Redux", "HTML", "CSS", "Bootstrap"],
         time: "June 2021 - July 2021 (2 months)",
     },
 ];
