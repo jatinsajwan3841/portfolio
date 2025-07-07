9566cddeac85ce2006ddc7bd629778bd13597eee
jatinsajwan3841 <jatinsajwan3841@users.noreply.github.com>
2025-07-07 09:46:28 +0000

    deploy: 3560b5172c077d68aac10d44854b05fe7a20c645

---
+++ assets/index-Cco7RvXL.js
@@ -0,0 +1,13 @@
+import{j as e}from"./index-Ckv9tSy0.js";import{I as s}from"./index-o9Nyy4rE.js";/* empty css              */const o=[{role:"Systems Engineer",company:"TCS Digital",desc:`◦ Worked on end-to-end migration of the Xpert platform from AngularJS to ReactJS, improving performance
+and user experience for a globally-used recruiting tool.
+◦ Developed and maintained frontend components using JavaScript, SCSS, HTML5, and ReactJS, ensuring
+cross-browser compatibility and responsiveness.
+◦ Collaborated in an Agile environment, participating in daily stand-ups, sprint planning, and code reviews
+to deliver high-quality features on time.
+◦ Conducted browser-based debugging and Eclipse debugging to identify and resolve critical issues, improving
+application stability.
+◦ Managed incidents by investigating issues, applying fixes, and maintaining up-time for users across the
+globe.
+◦ Developed new APIs in Java to support platform enhancements, expanding functionality and streamlining
+processes for recruiters.
+◦ Utilized tools like VS Code, Eclipse, and Postman for efficient coding, debugging, and API testing.`,tech:["ReactJs","JavaScript","Java","PostgreSQL","Redux","HTML","CSS"],time:"August 2022 - June 2025"},{role:"Software Development Intern",company:"Evolv Fit",desc:"It was 2 months long remote internship in which I developed dashboard and integrated mediapipe solutions in React. The dashboard will help the users to stay fit by providing all key metrics and relevant details. With the mediapipe solutions, although the model was developed by ML team in python, I integrated it in React which will help users to have guided exercises.",tech:["ReactJs","JavaScript","Redux","HTML","CSS","Bootstrap"],time:"June 2021 - July 2021 (2 months)"}],c=()=>e.jsx("div",{className:"work-container projects-container",children:o.map((i,n)=>e.jsx("div",{className:"project-item",children:e.jsxs("div",{className:"left",children:[e.jsx("div",{className:"title",children:i.role}),e.jsxs("div",{className:"company",children:["@ ",i.company]}),e.jsx("div",{className:"desc",dangerouslySetInnerHTML:{__html:i.desc}}),e.jsx("div",{className:"tech-used",children:i.tech.map((t,a)=>e.jsx(s,{name:t},a))}),e.jsx("div",{className:"time",children:i.time})]})},n))});export{c as default};
